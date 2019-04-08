6230bdf99b6be4c06f383108a5ab99882a0b1fba
ta-miyasaka <ta-miyasaka@kddi-research.jp>
2019-04-08 14:03:26 +0900

    seg6 update

--- ipt_NETFLOW.c
+++ ipt_NETFLOW.c
@@ -5132,9 +5132,10 @@ do_protocols:
 		    case IPPROTO_ICMPV6: {
 			struct icmp6hdr _icmp6h, *ic;
 			if (likely(family == AF_INET6) &&
-				    likely(ic = skb_header_pointer(skb, ptr, 2, &_icmp6h)))
+				    likely(ic = skb_header_pointer(skb, ptr, 2, &_icmp6h))) {
 				tuple.d_port = htons((ic->icmp6_type << 8) | ic->icmp6_code);
 				printk(KERN_INFO "ipt_NETFLOW : [debug] ICMPv6 type : %d\n",ic->icmp6_type);
+                }   
 			break;
 		    }
 		    case IPPROTO_IGMP: {
